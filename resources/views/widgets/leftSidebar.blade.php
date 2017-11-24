7928e1194718d993d7aab1d9e8aa218aaaecead3
MSST <leo2120@yandex.ru>
2017-11-24 14:31:35 +0300

    Misc stuff

--- resources/views/widgets/leftSidebar.blade.php
+++ resources/views/widgets/leftSidebar.blade.php
@@ -1,5 +1,4 @@
-<p>Just left sidebar</p><br>
-Что здесь можно поместить?<br><br>
+<p>Just left sidebar</p><br><br>
 Категории:<br>
 @foreach($categories as $category)
   <a href="/category/{{ $category->categoryName }}">{{ $category->categoryName }}</a><br><br>
--- resources/views/widgets/postCard.blade.php
+++ resources/views/widgets/postCard.blade.php
@@ -1,4 +1,4 @@
-<a href="posts/{{$post->id}}"><div class="item">
+<a href="/posts/{{$post->id}}"><div class="item">
   <div class="thumbnail" style="min-height:220px; min-width:120px">
     @if( isset($post->image) )
     <img src="{{ asset('storage/' . $post->image) }}" alt="" class="img-responsive">
