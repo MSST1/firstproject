91dd35a4a5af5d2d8679cd8ea39a5cd69dd63df6
MSST <leo2120@yandex.ru>
2017-11-27 13:58:29 +0300

    Slicing into parts stage 1

---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,5 @@
+<div class="logo">
+  <a href="#">zVintauge</a>
+  <span>Collectible Vintage & Antique Photos</span>
+</div>
+@include('widgets.__menu')
---
+++ resources/views/widgets/__CSS.blade.php
@@ -0,0 +1,9 @@
+<!-- CSS
+================================================== -->
+<link rel="stylesheet" href="css/zerogrid.css">
+<link rel="stylesheet" href="css/style.css">
+<!-- Custom Fonts -->
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/menu.css">
+<!-- Owl Carousel Assets -->
+<link href="owl-carousel/owl.carousel.css" rel="stylesheet">
