449c42e8e0e1b8b71267e645f512e9d4edb72b22
MSST <leo2120@yandex.ru>
2017-11-28 11:20:50 +0300

    Contact page changes

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -14,26 +14,26 @@
           {{ csrf_field() }}
           <label class="row">
 						<div class="col-1-2">
+							@if ($errors->has('author'))
+								<div class="alert alert-danger text-center">{{ $errors->first('author') }}</div>
+							@endif
 							<div class="wrap-col">
-								<input type="text" name="author" id="name" placeholder="Как к вам обращаться?*" />
+								<input type="text" name="author" id="author" placeholder="Как к вам обращаться?*" value="{{ old('author') }}"/>
 							</div>
-              @if ($errors->has('author'))
-                <div class="alert alert-danger text-center">{{ $errors->first('author') }}</div>
-              @endif
 						</div>
 						<div class="col-1-2">
+							@if ($errors->has('email'))
+								<div class="alert alert-danger text-center">{{ $errors->first('email') }}</div>
+							@endif
 							<div class="wrap-col">
-								<input type="text" name="email" id="email" placeholder="Ваш email*"/>
+								<input type="text" name="email" id="email" placeholder="Ваш email*" value="{{ old('email') }}"/>
 							</div>
-              @if ($errors->has('email'))
-                <div class="alert alert-danger text-center">{{ $errors->first('email') }}</div>
-              @endif
 						</div>
 					</label>
 					<label class="row">
 						<div class="wrap-col">
-							<textarea name="text" id="message" class="form-control" rows="4" cols="25"
-							placeholder="Текст обращения*"></textarea>
+							<textarea name="text" id="text" class="form-control" rows="4" cols="25"
+							placeholder="Текст обращения*">{{ old('text') }}</textarea>
 						</div>
             @if ($errors->has('text'))
               <div class="alert alert-danger text-center">{{ $errors->first('text') }}</div>
@@ -44,4 +44,25 @@
 			</div>
 		</div>
 </section>
+@endsection
+
+@section('JS')
+	@if($errors->has('author'))
+		<script>
+			let erroredAuth = document.getElementById('author');;
+			erroredAuth.classList.add("field-validation-error");
+		</script>
+	@endif
+	@if($errors->has('email'))
+	<script>
+		let erroredEmail = document.getElementById('email');
+		erroredEmail.classList.add("field-validation-error");
+	</script>
+	@endif
+	@if($errors->has('text'))
+	<script>
+		let erroredText = document.getElementById('text');
+		erroredText.classList.add("field-validation-error");
+	</script>
+	@endif
 @endsection
