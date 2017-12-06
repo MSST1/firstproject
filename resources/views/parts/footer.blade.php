f5486375b077c5fb2f4be4726a073606aa30880b
MSST <leo2120@yandex.ru>
2017-12-06 10:20:46 +0300

    Footer changes

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,77 +1,17 @@
 <div class="zerogrid wrap-footer">
   <div class="row">
-    <div class="col-1-4 col-footer-1">
+    <div class="col-1-2 col-footer-1">
       <div class="wrap-col">
-        <h3 class="widget-title">About Us</h3>
-        <p>Ut volutpat consectetur aliquam. Curabitur auctor in nis ulum ornare. Metus elit vehicula dui. Curabitur auctor in nis ulum ornare. Sed consequat, augue condimentum fermentum</p>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque la udantium</p>
+      <center><h3 class="widget-title">Обо мне</h3></center>
+        <p>
+          Да, в общем-то, сделал тут бложик для тренировки, ничего интересного.
+        </p>
       </div>
     </div>
-    <div class="col-1-4 col-footer-2">
+    <div class="col-1-2 col-footer-3">
       <div class="wrap-col">
-        <h3 class="widget-title">Recent Post</h3>
-        <ul>
-          <li><a href="#">MOST VISITED COUNTRIES</a></li>
-          <li><a href="#">5 PLACES THAT MAKE A GREAT HOLIDAY</a></li>
-          <li><a href="#">PEBBLE TIME STEEL IS ON TRACK TO SHIP IN JULY</a></li>
-          <li><a href="#">STARTUP COMPANY???S CO-FOUNDER TALKS ON HIS NEW PRODUCT</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="col-1-4 col-footer-3">
-      <div class="wrap-col">
-        <h3 class="widget-title">Tag Cloud</h3>
-        <a href="#">animals</a>
-        <a href="#">cooking</a>
-        <a href="#">countries</a>
-        <a href="#">city</a>
-        <a href="#">children</a>
-        <a href="#">home</a>
-        <a href="#">likes</a>
-        <a href="#">photo</a>
-        <a href="#">link</a>
-        <a href="#">law</a>
-        <a href="#">shopping</a>
-        <a href="#">skate</a>
-        <a href="#">scholl</a>
-        <a href="#">video</a>
-        <a href="#">travel</a>
-        <a href="#">images</a>
-        <a href="#">love</a>
-        <a href="#">lists</a>
-        <a href="#">makeup</a>
-        <a href="#">media</a>
-        <a href="#">password</a>
-        <a href="#">pagination</a>
-        <a href="#">wildlife</a>
-      </div>
-    </div>
-    <div class="col-1-4 col-footer-4">
-      <div class="wrap-col">
-        <h3 class="widget-title">Gallery</h3>
-        <div class="row">
-          <div class="col-1-3">
-            <div class="wrap-col">
-              <a href="#"><img src="images/11.jpg"></a>
-              <a href="#"><img src="images/1.jpg"></a>
-              <a href="#"><img src="images/13.jpg"></a>
-            </div>
-          </div>
-          <div class="col-1-3">
-            <div class="wrap-col">
-              <a href="#"><img src="images/10.jpg"></a>
-              <a href="#"><img src="images/9.jpg"></a>
-              <a href="#"><img src="images/4.jpg"></a>
-            </div>
-          </div>
-          <div class="col-1-3">
-            <div class="wrap-col">
-              <a href="#"><img src="images/2.jpg"></a>
-              <a href="#"><img src="images/6.jpg"></a>
-              <a href="#"><img src="images/5.jpg"></a>
-            </div>
-          </div>
-        </div>
+      <center><h3 class="widget-title">Облако тегов</h3>
+        <a href="#">Sample tag</a></center>
       </div>
     </div>
   </div>
