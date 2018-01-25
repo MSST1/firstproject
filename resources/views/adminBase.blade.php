4a6af0ffdb787a8676404c8dce4be21e7786a697
MSST <leo2120@yandex.ru>
2018-01-25 14:36:30 +0300

    Admin layout first steps

---
+++ resources/views/adminBase.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>adminpage</title>
+    <link rel="stylesheet" href={{asset('css/adminStyles.css')}}>
+  </head>
+  <body>
+    <div class="menu">
+      topqeq<br>lmao
+    </div>
+    <div class="topMenu">
+      topmenu
+    </div>
+    <div class="content">
+          @yield('content')
+    </div>
+
+  </body>
+</html>
