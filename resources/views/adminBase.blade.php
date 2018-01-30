34f46e4dedd5fd4f4d53e35922a623e7812d39c9
MSST <leo2120@yandex.ru>
2018-01-30 11:59:02 +0300

    AdminPanel second invasion

--- resources/views/adminBase.blade.php
+++ resources/views/adminBase.blade.php
@@ -4,10 +4,22 @@
     <meta charset="utf-8">
     <title>adminpage</title>
     <link rel="stylesheet" href={{asset('css/adminStyles.css')}}>
+    <link rel="stylesheet" href={{asset('font-awesome/css/font-awesome.css')}}>
   </head>
   <body>
     <div class="menu">
-      topqeq<br>lmao
+      <div class="logo">
+      </div>
+      <div class="sidebarMenu">
+        <div class="menuItem">
+          <span class="fa fa-user fa-2x"></span><br>
+          users
+        </div>
+        <div class="menuItem">
+          <span class="fa fa-trash fa-2x"></span><br>
+          posts
+        </div>
+      </div>
     </div>
     <div class="topMenu">
       topmenu
